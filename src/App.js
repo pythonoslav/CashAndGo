76f2726077e47c93da56308ed1d7bb0c0f08d77b
klinski1 <evgenijkislinskij558@gmail.com>
2025-01-12 00:15:47 +0500

    add reseving cash

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 import FeaturesSection from "./components/FeaturesSection/FeaturesSection";
 import DropCashSection from "./components/DropCashSection/DropCashSections";
+import ResivingCash from "./components/RecivingCash/ResivingCash";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <HeroSection />
       <FeaturesSection />
       <DropCashSection/>
+      <ResivingCash/>
     </>
   );
 };
---
+++ src/components/RecivingCash/ResivingCash.jsx
@@ -0,0 +1,141 @@
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Box, IconButton, Typography } from "@mui/material";
+import "swiper/css";
+import { ReactComponent as RecivingCash } from "../../assets/Receiving_cash.svg";
+import { ReactComponent as ArrowButtonRight } from "../RecivingCash/assets/ArrowButton.svg";
+import { ReactComponent as ArrowButtonLeft } from "../RecivingCash/assets/ArrowButtonBack.svg";
+
+const images = [
+    "/images/photo1.png",
+    "/images/photo2.png",
+    "/images/photo3.png",
+    "/images/photo1.png",
+    "/images/photo2.png",
+    "/images/photo3.png",
+];
+
+const CustomCarousel = () => {
+    const swiperRef = useRef(null);
+
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                maxWidth: "1400px",
+                margin: "0 auto",
+                position: "relative",
+                overflow: "hidden",
+            }}
+        >
+
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignItems: "flex-start",
+                    mb: "30px",
+                    height: '1024',
+                }}
+            >
+                <RecivingCash sx={{width:'750px'}}/>
+            </Box>
+            <Typography
+                variant="body1"
+                sx={{
+                    fontSize: "18px",
+                    fontWeight: "300",
+                    lineHeight: "24px",
+                    color: "#0E1111",
+                    marginBottom: "16px",
+                    width: "40%",
+                    marginBottom: "2rem",
+                    '@media (max-width: 768px)': {
+                        fontSize: "16px", // Уменьшаем шрифт на мобильных
+                        lineHeight: "22px",
+                        textAlign: "left"
+                    },
+                }}
+            >Это офис с динамичной энергией и атмосферой, сочетающий эффективность, эстетику, комфорт и инновации</Typography>
+
+            {/* Swiper */}
+            <Swiper
+                onSwiper={(swiper) => (swiperRef.current = swiper)}
+                slidesPerView={1.5} // Показываем центральный слайд + части соседних
+                centeredSlides={true} // Центрируем текущий слайд
+                loopedSlides={images.length}
+                spaceBetween={30} // Отступы между слайдами
+                loop={true} // Зацикленное переключение
+                initialSlide={1} // Начинаем со второго слайда
+                breakpoints={{
+                    // Адаптивность для разных экранов
+                    640: { slidesPerView: 1.1, spaceBetween: 20 },
+                    1024: { slidesPerView: 1.5, spaceBetween: 30 },
+                }}
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <Box
+                            sx={{
+                                width: "100%",
+                                height: "600px",
+                                backgroundImage: `url(${image})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                borderRadius: "30px",
+                                boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)", // Тень для слайда
+                            }}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+
+            {/* Кнопки переключения */}
+            <IconButton
+                onClick={() => swiperRef.current?.slidePrev()}
+                sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "90px",
+                    color: "#fff",
+                    zIndex: 2,
+                }}
+            >
+                <ArrowButtonLeft />
+            </IconButton>
+            <IconButton
+                onClick={() => swiperRef.current?.slideNext()}
+                sx={{
+                    position: "absolute",
+                    top: "50%",
+                    right: "90px",
+                    color: "#fff",
+                    zIndex: 2,
+                }}
+            >
+                <ArrowButtonRight />
+            </IconButton>
+
+            <Typography variant="h3" sx={{
+                    fontSize: "36px",
+                    fontWeight: "900",
+                    lineHeight: "45px",
+                    color: "#0E1111",
+                    marginBottom: "16px",
+                    width: "70%",
+                    marginTop: '2rem',
+                    marginBottom: "2rem",
+                    marginLeft: '5ch',
+                    '@media (max-width: 768px)': {
+                        fontSize: "16px", // Уменьшаем шрифт на мобильных
+                        lineHeight: "22px",
+                        textAlign: "left"
+                    },
+                }}>Адрес: 5/27A, Fisherman Way, Moo 5 Wiset Rd, Rawai, Muang, Phuket 83130, Thailand</Typography>
+        </Box>
+
+
+    );
+};
+
+export default CustomCarousel;
