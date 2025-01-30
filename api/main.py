cd44abaa910229a650fc4b9626793c3cc7ad6d27
Pythonoslav <grendelb95@gmail.com>
2025-01-30 21:19:17 +0500

    fix

--- api/main.py
+++ api/main.py
@@ -5,7 +5,6 @@ from models.response_models import XeCurrencyConvertedListResponseModel
 from settings.config import get_settings
 from settings.mongo_config import save_currency_rates
 
-
 # Создание приложения FastAPI
 app = FastAPI()
 # Получение настроек
@@ -48,41 +47,14 @@ async def get_currency_exchange_rate():
         raise HTTPException(status_code=500,
                             detail="An unexpected error occurred.")
 
-
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    logger.error(f"HTTP Exception: {exc.detail}")
+    return await request.app.default_exception_handler(request, exc)
 
 # # Функция для запуска приложения
 # if __name__ == '__main__':
 #     import uvicorn
 #     uvicorn.run(app, host="127.0.0.1", port=8000)
 
-# import asyncio
-#
-# async def test():
-#     try:
-#         async with httpx.AsyncClient() as client:
-#             response = await client.get(
-#                 api_settings.exchange_rate_request,
-#                 auth=(api_settings.account_id, api_settings.api_key)
-#             )
-#
-#         response.raise_for_status()  # Проверяем успешность ответа
-#
-#         response_data = XeCurrencyConvertedListResponseModel(**response.json())
-#
-#         return {
-#             "is_error": False,
-#             "result": response_data.currencies_list
-#         }
-#
-#     except httpx.HTTPStatusError as e:
-#         logger.error(f"HTTP error: {e.response.status_code} - {e.response.text}")
-#         raise HTTPException(status_code=e.response.status_code,
-#                             detail="Failed to fetch currency exchange rate.")
-#
-#     except Exception as e:
-#         logger.exception("An unexpected error occurred")
-#         raise HTTPException(status_code=500,
-#                             detail="An unexpected error occurred.")
-#
-# print(asyncio.run(test()))
 
--- api/settings/mongo_config.py
+++ api/settings/mongo_config.py
@@ -7,8 +7,7 @@ class MongoDBSettings:
 
     @property
     def connection_string(self):
-        print(f"mongodb://{self.settings.mongo_user}:{self.settings.mongo_password}@localhost:27017/{self.settings.mongo_database}")
-        return f"mongodb://{self.settings.mongo_user}:{self.settings.mongo_password}@localhost:27017/{self.settings.mongo_database}"
+        return f"mongodb://{self.settings.mongo_user}:{self.settings.mongo_password}@localhost:27017/"
 
 class MongoDBClient:
     def __init__(self):
@@ -19,40 +18,15 @@ class MongoDBClient:
         return self.db[collection_name]
 
 
-async def save_currency_rates(currencies_list):
+async def save_currency_rates(currencies_list: list):
     """Сохраняет курсы валют в базу данных MongoDB."""
     # Удаляем старые данные
     mongo_client = MongoDBClient()
-    await mongo_client.get_collection("currencies").drop()
+    collection = await mongo_client.get_collection("exchange_rates")
 
-    # Вставляем новые данные
     for currency in currencies_list:
-        await mongo_client.get_collection("currencies").insert_one({
-            "quotecurrency": currency.quotecurrency,
-            "mid": currency.mid
-        })
-    await mongo_client.get_collection("currencies").delete_many({})
-
-    # Вставляем новые данные
-    for currency in currencies_list:
-        await mongo_client.get_collection("currencies").insert_one({
-            "quotecurrency": currency.quotecurrency,
-            "mid": currency.mid
-        })
-
-# import asyncio
-#
-# async def fetch_currencies():
-#     mongo_client = MongoDBClient()
-#     collection = await mongo_client.get_collection("currencies")
-#
-#     # Запрос всех документов из коллекции
-#     currencies = await collection.find().to_list(length=None)
-#
-#     for currency in currencies:
-#         print(currency)
-#
-#
-# # Запуск асинхронной функции
-# if __name__ == "__main__":
-#     asyncio.run(fetch_currencies())
+        await collection.update_one(
+            {"quotecurrency": currency},  # Поиск по валюте
+            {"$set": {"mid": currency}},  # Обновляем mid
+            upsert=True  # Создаёт новую запись, если не найдётся существующей
+        )
