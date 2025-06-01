6ac35834a73995f36642df93382f8d5a57c5c2af
Klinski1 <evgenijkislimskij558@gmail.com>
2025-06-01 22:14:26 +0500

    add path to rotes for admin panel

--- frontend-admin/src/routes/LoginPage.jsx
+++ frontend-admin/src/routes/LoginPage.jsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('/api/auth/sign_in', {
         email,
         password,
       });
