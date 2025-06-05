6efde25c70d8e4777f283547a7aa8022f4694c28
Klinski1 <evgenijkislimskij558@gmail.com>
2025-06-06 01:31:12 +0500

    try last version

--- frontend-admin/vite.config.js
+++ frontend-admin/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './admin-panel/', // Указываем подкаталог
+  base: '/admin-panel/', // Абсолютный путь для соответствия basename
   server: {
     host: '0.0.0.0',
     port: 3002,
   },
   build: {
-    outDir: 'dist', // Убедимся, что выходная папка — dist
+    outDir: 'dist', // Выходная папка
   },
 });
