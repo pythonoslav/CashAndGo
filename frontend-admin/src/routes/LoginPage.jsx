493cf36e5aabcf9e39d121379960bf8ea4774fd2
Klinski1 <evgenijkislimskij558@gmail.com>
2025-06-07 03:24:36 +0500

    try last version

--- frontend-admin/src/routes/LoginPage.jsx
+++ frontend-admin/src/routes/LoginPage.jsx
@@ -56,7 +56,7 @@ export default function LoginPage() {
     }
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('/api/auth/login', {
         email,
         password,
       });
