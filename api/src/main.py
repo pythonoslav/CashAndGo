0f6c9feaf0201c4eb1cf764fa40b6e2d1f5ce440
pythonoslav <grendelb95@gmail.com>
2025-06-08 02:32:01 +0500

    cors

--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers.currencies import currencies_router
 from src.routers.auth import auth_router
@@ -6,6 +7,15 @@ from src.routers.rates import rates
 
 
 app = FastAPI(root_path="/api")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://cashandgo.exchange"],  # добавить все нужные адреса!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(currencies_router)
 app.include_router(auth_router)
