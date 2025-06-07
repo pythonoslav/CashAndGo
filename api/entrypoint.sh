9ab62c0e2df2a1a8eb03628cee7a0231096e1136
pythonoslav <grendelb95@gmail.com>
2025-06-08 03:29:51 +0500

    jeka zaebal dai pospat

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 export PYTHONPATH=/api
 python -m src.db_operations.database_init
-exec uvicorn src.main:app --host 0.0.0.0 --port 5000
+exec uvicorn src.main:app --host 0.0.0.0 --port 5000 --ssl-keyfile /api/certs/tls.key --ssl-certfile /api/certs/tls.crt
